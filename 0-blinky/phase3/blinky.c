22431dc2c1c4058c2c8383c44a43a88fbc4aed3c
Evan Cameron <evan@vectorface.com>
2018-07-26 16:00:08 -0400

    fsel_out (untested)

--- 0-blinky/phase3/blinky.c
+++ 0-blinky/phase3/blinky.c
@@ -1,6 +1,7 @@
 #define GPIO_BASE (0x3F000000 + 0x200000)
 
 volatile unsigned *GPIO_FSEL1 = (volatile unsigned *)(GPIO_BASE + 0x04);
+volatile unsigned *GPIO_FSEL0 = (volatile unsigned *)(GPIO_BASE);
 volatile unsigned *GPIO_SET0 = (volatile unsigned *)(GPIO_BASE + 0x1C);
 volatile unsigned *GPIO_CLR0 = (volatile unsigned *)(GPIO_BASE + 0x28);
 
@@ -20,9 +21,20 @@ static void spin_sleep_ms(unsigned int ms)
 {
   spin_sleep_us(ms * 1000);
 }
-static void fsel(unsigned int pin_num)
+
+static void fsel_out(unsigned int pin_num)
 {
-  unsigned int fsel_pos = (pin_num % 10) * 3;
+  // each 32-bit wide space fits 10 FSEL, and our mask is size 3
+  unsigned int shift = (pin_num % 10) * 3;
+  // to get which FSEL(n), integer div by 10 the pin number, as they are in chunks of 10
+  unsigned int offset = pin_num / 10;
+  GPIO_FSEL0[offset] |= (GPIO_OUT << shift);
+  return;
+}
+
+static void set_pin(unsigned int pin_num, unsigned int val)
+{
+  *GPIO_SET0 = val << pin_num;
 }
 // Q: Now, read the documentation for GPFSELn register on pages 91 and 92.
 // We write to this register to set up a pin as an output or input.
@@ -37,9 +49,9 @@ int main(void)
   // STEP 2: Continuously set and clear GPIO 16.
   while (1)
   {
-    *GPIO_SET0 = 1 << 16;
+    *GPIO_SET0 |= 1 << 16;
     spin_sleep_us(100);
-    *GPIO_SET0 = 1 << 16;
+    *GPIO_CLR0 |= 1 << 16;
     spin_sleep_us(100);
   }
 }
