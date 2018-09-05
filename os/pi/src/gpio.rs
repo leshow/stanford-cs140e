f74d7992c212dacac0f18cda98bbd33df1ee41bc
Evan Cameron <evan@vectorface.com>
2018-09-05 14:15:11 -0400

    os GPIO impl

--- os/pi/src/gpio.rs
+++ os/pi/src/gpio.rs
@@ -1,8 +1,8 @@
 use core::marker::PhantomData;
 
-use common::{IO_BASE, states};
+use common::{states, IO_BASE};
 use volatile::prelude::*;
-use volatile::{Volatile, WriteVolatile, ReadVolatile, Reserved};
+use volatile::{ReadVolatile, Reserved, Volatile, WriteVolatile};
 
 /// An alternative GPIO function.
 #[repr(u8)]
@@ -14,7 +14,7 @@ pub enum Function {
     Alt2 = 0b110,
     Alt3 = 0b111,
     Alt4 = 0b011,
-    Alt5 = 0b010
+    Alt5 = 0b010,
 }
 
 #[repr(C)]
@@ -58,14 +58,15 @@ states! {
 /// structure starts in the `Uninitialized` state and must be transitions into
 /// one of `Input`, `Output`, or `Alt` via the `into_input`, `into_output`, and
 /// `into_alt` methods before it can be used.
-pub struct Gpio<State> {
+pub struct Gpio<T> {
     pin: u8,
     registers: &'static mut Registers,
-    _state: PhantomData<State>
+    _state: PhantomData<T>,
 }
 
 /// The base address of the `GPIO` registers.
 const GPIO_BASE: usize = IO_BASE + 0x200000;
+const GPIO_MASK: u32 = 0b111;
 
 impl<T> Gpio<T> {
     /// Transitions `self` to state `S`, consuming `self` and returning a new
@@ -76,7 +77,7 @@ impl<T> Gpio<T> {
         Gpio {
             pin: self.pin,
             registers: self.registers,
-            _state: PhantomData
+            _state: PhantomData,
         }
     }
 }
@@ -95,14 +96,25 @@ impl Gpio<Uninitialized> {
         Gpio {
             registers: unsafe { &mut *(GPIO_BASE as *mut Registers) },
             pin: pin,
-            _state: PhantomData
+            _state: PhantomData,
         }
     }
 
     /// Enables the alternative function `function` for `self`. Consumes self
     /// and returns a `Gpio` structure in the `Alt` state.
     pub fn into_alt(self, function: Function) -> Gpio<Alt> {
-        unimplemented!()
+        let shift = (self.pin % 10) * 3;
+        let offset = self.pin / 10;
+        {
+            let gpio_pin = &mut self.registers.FSEL[offset as usize];
+            let gpio_cur: u32 = gpio_pin.read();
+            gpio_pin.write(gpio_cur & !(GPIO_MASK << shift) | ((function as u32) << shift));
+        }
+        Gpio {
+            pin: self.pin,
+            registers: self.registers,
+            _state: PhantomData::<Alt>,
+        }
     }
 
     /// Sets this pin to be an _output_ pin. Consumes self and returns a `Gpio`
@@ -121,12 +133,12 @@ impl Gpio<Uninitialized> {
 impl Gpio<Output> {
     /// Sets (turns on) the pin.
     pub fn set(&mut self) {
-        unimplemented!()
+        self.registers.SET[(self.pin / 32) as usize].write(1 << self.pin);
     }
 
     /// Clears (turns off) the pin.
     pub fn clear(&mut self) {
-        unimplemented!()
+        self.registers.CLR[(self.pin / 32) as usize].write(1 << self.pin);
     }
 }
 
@@ -134,6 +146,6 @@ impl Gpio<Input> {
     /// Reads the pin's value. Returns `true` if the level is high and `false`
     /// if the level is low.
     pub fn level(&mut self) -> bool {
-        unimplemented!()
+        self.registers.LEV[(self.pin / 32) as usize].has_mask(1 << self.pin)
     }
 }
