718fc1ff82a312e233fbfa473096cd4c9e7b976f
Evan Cameron <evan@vectorface.com>
2018-09-05 12:43:29 -0400

    Timer implementation

--- os/pi/src/timer.rs
+++ os/pi/src/timer.rs
@@ -1,6 +1,6 @@
 use common::IO_BASE;
 use volatile::prelude::*;
-use volatile::{Volatile, ReadVolatile};
+use volatile::{ReadVolatile, Volatile};
 
 /// The base address for the ARM system timer registers.
 const TIMER_REG_BASE: usize = IO_BASE + 0x3000;
@@ -11,12 +11,12 @@ struct Registers {
     CS: Volatile<u32>,
     CLO: ReadVolatile<u32>,
     CHI: ReadVolatile<u32>,
-    COMPARE: [Volatile<u32>; 4]
+    COMPARE: [Volatile<u32>; 4],
 }
 
 /// The Raspberry Pi ARM system timer.
 pub struct Timer {
-    registers: &'static mut Registers
+    registers: &'static mut Registers,
 }
 
 impl Timer {
@@ -30,21 +30,26 @@ impl Timer {
     /// Reads the system timer's counter and returns the 64-bit counter value.
     /// The returned value is the number of elapsed microseconds.
     pub fn read(&self) -> u64 {
-        unimplemented!()
+        let low = self.registers.CLO.read() as u64;
+        let high = self.registers.CHI.read() as u64;
+        high << 32 | low
     }
 }
 
 /// Returns the current time in microseconds.
 pub fn current_time() -> u64 {
-    unimplemented!()
+    Timer::new().read()
 }
 
 /// Spins until `us` microseconds have passed.
 pub fn spin_sleep_us(us: u64) {
-    unimplemented!()
+    let timer = Timer::new();
+    let until = timer.read() + us;
+
+    while timer.read() < until {}
 }
 
 /// Spins until `ms` milliseconds have passed.
 pub fn spin_sleep_ms(ms: u64) {
-    unimplemented!()
+    spin_sleep_us(ms * 1000);
 }
