a95d6ffe72613d5c50108f6ab1b09d9ecbf6a0b5
Evan Cameron <cameron.evan@gmail.com>
2018-09-08 02:02:16 -0400

    Bounded FnMut in struct

--- 1-shell/xmodem/src/lib.rs
+++ 1-shell/xmodem/src/lib.rs
@@ -18,14 +18,20 @@ const CAN: u8 = 0x18;
 const MASK: u8 = 0b00000011;
 
 /// Implementation of the XMODEM protocol.
-pub struct Xmodem<R> {
+pub struct Xmodem<R, F>
+where
+    F: FnMut(Progress),
+{
     packet: u8,
     inner: R,
     started: bool,
-    progress: ProgressFn,
+    progress: F,
 }
 
-impl Xmodem<()> {
+impl<F> Xmodem<(), F>
+where
+    F: FnMut(Progress),
+{
     /// Transmits `data` to the receiver `to` using the XMODEM protocol. If the
     /// length of the total data yielded by `data` is not a multiple of 128
     /// bytes, the data is padded with zeroes and sent to the receiver.
@@ -48,7 +54,7 @@ impl Xmodem<()> {
     /// the transmission. See the [`Progress`] enum for more information.
     ///
     /// Returns the number of bytes written to `to`, excluding padding zeroes.
-    pub fn transmit_with_progress<R, W>(mut data: R, to: W, f: ProgressFn) -> io::Result<usize>
+    pub fn transmit_with_progress<R, W>(mut data: R, to: W, f: F) -> io::Result<usize>
     where
         W: io::Read + io::Write,
         R: io::Read,
@@ -96,7 +102,7 @@ impl Xmodem<()> {
     ///
     /// The function `f` is used as a callback to indicate progress throughout
     /// the reception. See the [`Progress`] enum for more information.
-    pub fn receive_with_progress<R, W>(from: R, mut into: W, f: ProgressFn) -> io::Result<usize>
+    pub fn receive_with_progress<R, W>(from: R, mut into: W, f: F) -> io::Result<usize>
     where
         R: io::Read + io::Write,
         W: io::Write,
@@ -125,7 +131,7 @@ impl Xmodem<()> {
     }
 }
 
-impl<T: io::Read + io::Write> Xmodem<T> {
+impl<T: io::Read + io::Write, F: FnMut(Progress)> Xmodem<T, F> {
     /// Returns a new `Xmodem` instance with the internal reader/writer set to
     /// `inner`. The returned instance can be used for both receiving
     /// (downloading) and sending (uploading).
@@ -143,7 +149,7 @@ impl<T: io::Read + io::Write> Xmodem<T> {
     /// (downloading) and sending (uploading). The function `f` is used as a
     /// callback to indicate progress throughout the transfer. See the
     /// [`Progress`] enum for more information.
-    pub fn new_with_progress(inner: T, f: ProgressFn) -> Self {
+    pub fn new_with_progress(inner: T, f: F) -> Self {
         Xmodem {
             packet: 1,
             started: false,
