af70398ba55110afe3fcff4de3aeae6141262a71
Evan Cameron <cameron.evan@gmail.com>
2018-09-22 23:26:19 -0400

    start shell impl

--- os/kernel/src/shell.rs
+++ os/kernel/src/shell.rs
@@ -1,16 +1,16 @@
-use stack_vec::StackVec;
 use console::{kprint, kprintln, CONSOLE};
+use stack_vec::StackVec;
 
 /// Error type for `Command` parse failures.
 #[derive(Debug)]
 enum Error {
     Empty,
-    TooManyArgs
+    TooManyArgs,
 }
 
 /// A structure representing a single shell command.
 struct Command<'a> {
-    args: StackVec<'a, &'a str>
+    args: StackVec<'a, &'a str>,
 }
 
 impl<'a> Command<'a> {
@@ -36,12 +36,46 @@ impl<'a> Command<'a> {
 
     /// Returns this command's path. This is equivalent to the first argument.
     fn path(&self) -> &str {
-        unimplemented!()
+        self.args[0]
     }
 }
 
+const WELCOME: &'static str = r#"
+                                                         c=====e
+                                                            H
+   ____________                                         _,,_H__
+  (__((__((___()                                       //|     |
+ (__((__((___()()_____________________________________// |ACME |
+(__((__((___()()()------------------------------------'  |_____|
+"#;
+
+const DEL: u8 = 127;
+const BKSP: u8 = 8;
+const BELL: u8 = 7;
 /// Starts a shell using `prefix` as the prefix for each line. This function
 /// never returns: it is perpetually in a shell loop.
 pub fn shell(prefix: &str) -> ! {
-    unimplemented!()
+    kprintln!("{}", WELCOME);
+    let mut hist_buf = [0u8; 512];
+    let mut history = StackVec::new(&mut hist_buf);
+
+    loop {
+        kprint!("{}", prefix);
+        let mut cmd_buf = [0u8; 512];
+        let mut cmd = StackVec::new(&mut cmd_buf);
+        loop {
+            let byte = CONSOLE.lock().read_byte();
+            match byte {
+                b'\r' | b'\n' => {}
+                BKSP => {
+                    if cmd.pop().is_none() {
+                        CONSOLE.lock().write_byte(BELL);
+                    }
+                }
+                _ => {
+                    cmd.push(byte);
+                }
+            }
+        }
+    }
 }
