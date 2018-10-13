c4a394de4ae6e8b87e328b9f35f93d3c9b14a2a3
Evan Cameron <cameron.evan@gmail.com>
2018-10-13 16:26:55 -0400

    implement bump allocator

--- os/kernel/src/allocator/bump.rs
+++ os/kernel/src/allocator/bump.rs
@@ -13,7 +13,10 @@ impl Allocator {
     /// Creates a new bump allocator that will allocate memory from the region
     /// starting at address `start` and ending at address `end`.
     pub fn new(start: usize, end: usize) -> Allocator {
-        unimplemented!("bump allocator")
+        Allocator {
+            current: start,
+            end,
+        }
     }
 
     /// Allocates memory. Returns a pointer meeting the size and alignment
@@ -37,7 +40,13 @@ impl Allocator {
     /// (`AllocError::Exhausted`) or `layout` does not meet this allocator's
     /// size or alignment constraints (`AllocError::Unsupported`).
     pub fn alloc(&mut self, layout: Layout) -> Result<*mut u8, AllocErr> {
-        unimplemented!("bump allocation")
+        let cur = align_up(self.current, layout.align()).saturating_add(layout.size());
+        if cur > self.end {
+            Err(AllocErr::Exhausted { request: layout })
+        } else {
+            self.current = cur;
+            Ok((cur - layout.size()) as *mut u8)
+        }
     }
 
     /// Deallocates the memory referenced by `ptr`.
@@ -53,7 +62,5 @@ impl Allocator {
     ///
     /// Parameters not meeting these conditions may result in undefined
     /// behavior.
-    pub fn dealloc(&mut self, _ptr: *mut u8, _layout: Layout) {
-        unimplemented!("bump deallocation")
-    }
+    pub fn dealloc(&mut self, _ptr: *mut u8, _layout: Layout) {}
 }
