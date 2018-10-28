53877433efa572b34a9af819b58516de80be6ce4
Evan Cameron <cameron.evan@gmail.com>
2018-10-27 23:14:09 -0400

    2-fs fat.rs impl

--- 2-fs/fat32/src/lib.rs
+++ 2-fs/fat32/src/lib.rs
@@ -1,15 +1,16 @@
+#![allow(dead_code, unused_imports)]
 #![feature(decl_macro, conservative_impl_trait)]
 #![allow(safe_packed_borrows)]
 
-#[cfg(not(target_endian="little"))]
+#[cfg(not(target_endian = "little"))]
 compile_error!("only little endian platforms supported");
 
+mod mbr;
 #[cfg(test)]
 mod tests;
-mod mbr;
 mod util;
 
-pub mod vfat;
 pub mod traits;
+pub mod vfat;
 
 pub use mbr::*;
--- 2-fs/fat32/src/vfat/fat.rs
+++ 2-fs/fat32/src/vfat/fat.rs
@@ -16,7 +16,7 @@ pub enum Status {
     Bad,
     /// The FAT entry corresponds to a valid data cluster. The corresponding
     /// cluster is the last in its chain.
-    Eoc(u32)
+    Eoc(u32),
 }
 
 #[repr(C, packed)]
@@ -25,7 +25,16 @@ pub struct FatEntry(pub u32);
 impl FatEntry {
     /// Returns the `Status` of the FAT entry `self`.
     pub fn status(&self) -> Status {
-        unimplemented!("FatEntry::status()")
+        match self.0 & 0x0FFF_FFFF {
+            0x0000_0000 => Free,
+            0x0000_0001 => Reserved,
+            clus @ 0x0000_0002...0x0FFF_FFEF => Data(Cluster::from(clus)),
+            clus @ 0x0FFF_FFF0...0x0FFF_FFF5 => Data(Cluster::from(clus)),
+            0x0FFF_FFF6 => Reserved,
+            0x0FFF_FFF7 => Bad,
+            marker @ 0x0FFF_FFF8...0xFFFF_FFFF => Eoc(marker),
+            _ => unreachable!(),
+        }
     }
 }
 
